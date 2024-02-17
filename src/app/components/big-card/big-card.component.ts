d771dda772bdf165cbe9725afde645e472524e56
AsacEt <abraao.azvdo2@gmail.com>
2024-02-16 23:42:38 -0300

    content created/version1.0

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  cardAuthor:string=""
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string =""
+  @Input()
+  cardDescription:string =""
+  Id:string="0"
   constructor() { }
 
   ngOnInit(): void {
